df59f64f2d45fd6d69f73c642a12e441709d7a50
davidgarcial <d.alex10092@gmail.com>
2021-05-27 21:39:48 -0500

    Initialize account service

---
+++ AFORO255.AZURE/AFORO255.AZURE.Account/Models/Account.cs
@@ -0,0 +1,11 @@
+﻿namespace AFORO255.AZURE.Account.Models
+{
+    public class Account
+    {
+        public int IdAccount { get; set; }
+        public decimal TotalAmount { get; set; }
+        public int IdCustomer { get; set; }
+        public string FullName { get; set; }
+
+    }
+}
---
+++ AFORO255.AZURE/AFORO255.AZURE.Account/Repositories/IAccountRepository.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AFORO255.AZURE.Account.Repositories
+{
+    public class IAccountRepository
+    {
+    }
+}
